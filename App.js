62bc6b1264f5b8fdde7cb8214bab15d9a022e293
fl4pper <sr492743@dal.ca>
2024-05-21 19:16:41 -0300

    imported dependencies

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
+import MasonryList from '@react-native-seoul/masonry-list';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function App() {
   useDeviceContext(tw);
