8b650a0f6c16445db324e5887a16a187c1c70df9
fl4pper <sr492743@dal.ca>
2024-05-28 17:20:29 -0300

    starter code retrieved from session 3

--- App.js
+++ App.js
@@ -1,25 +1,94 @@
-import { SafeAreaView, Text } from 'react-native';
+import { useEffect, useLayoutEffect } from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import 'react-native-reanimated'; 
-import MasonryList from '@react-native-seoul/masonry-list';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MasonryList from '@react-native-seoul/masonry-list'
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation } from './db';
 
-function App() {
+function HomeScreen({ navigation }) {
+  const { data: searchData, error, isLoading } = useSearchNotesQuery("");
+  const [ addNote, { data: addNoteData, error: addNoteError }] = useAddNoteMutation();
+  const [ deleteNote ] = useDeleteNoteMutation();
+  
+  useEffect(() => {
+    if (addNoteData != undefined) {
+      console.log(addNoteData.title);
+      navigation.navigate("Edit", {data: addNoteData});
+    }
+  }, [addNoteData]);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => deleteNote(item) } style={tw`w-[98%] mb-0.5 mx-auto bg-purple-300 rounded-sm px-1`}> 
+      <Text>{item.title} {item.id}</Text>
+    </TouchableOpacity>
+  )
+
+  return (
+    <View style={tw`flex-1 items-center justify-center bg-purple-400`}>
+      {searchData ? 
+        <MasonryList
+          style={tw`px-0.5 pt-0.5 pb-20`}
+          data={searchData}
+          numColumns={2}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+        />  
+        : <></>
+      }
+      <TouchableOpacity onPress={() => { addNote({title: "test", content: "content"}); }} style={tw`bg-blue-500 rounded-full absolute bottom-[5%] right-8 mx-auto items-center flex-1 justify-center w-12 h-12`}>
+        <Text style={tw`text-white text-center text-3xl mt--1`}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function EditScreen({ route, navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: route.params.data.title });
+  }, []);
+
+  return (
+    <View style={tw`flex-1 items-center justify-center bg-purple-400`}>
+      <Text style={tw`text-lg text-white`}>Edit Screen {route.params.data.title} {route.params.data.id}</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   useDeviceContext(tw);
 
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <Text style={tw`w-screen mt-16 text-center text-xl`}>
-          Your app code goes here.
-        </Text>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            options={{
+              headerStyle: tw`bg-purple-300 border-0`,
+              headerTintColor: '#fff',
+              headerTitleStyle: tw`font-bold`,
+              headerShadowVisible: false, // gets rid of border on device
+            }}
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{
+              headerStyle: tw`bg-purple-300 border-0`,
+              headerTintColor: '#fff',
+              headerTitleStyle: tw`font-bold`,
+              headerShadowVisible: false, // gets rid of border on device
+            }}
+            name="Edit"
+            component={EditScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
-  )
+  );
 }
-
-export default App;
